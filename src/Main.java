9ed0c35be722e8554c5f396bb742761f1b180ac5
Aikel <aikelen.030904@gmail.com>
2024-08-23 02:22:42 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 33333;
+        byte b = 111;
+        short c = 5555;
+        long d = 22222222;
+        float e = 6666.6666f;
+        double f = 77777.777777d;
+
+
+        float z = 27.12f;
+        long q = 987678965549L;
+        float x = 2.786f;
+        short w = 569;
+        short t = -159;
+        short y = 27897;
+        byte g = 67;
+
+        System.out.println("На каждого ученика выделено " + 480 / (23 + 27 + 30) + " листов бумаги");
+
+        System.out.println("За 20 минут "+ 20 * 1);
+        System.out.println("За сутки " + 24 * 60);
+        System.out.println("За 3 дня " + 24 * 60 * 3);
+        System.out.println("За 1 месяц " + 24 * 60 * 30);
+
+
+        System.out.println("В школе, где "+ 120 / (2 + 4) +" классов, нужно " + 120 / 2 + " банок белой краски и " + 120 / 4 + " банок коричневой краски");
+
+        int Breakfast = 5 * 80 + 210 + 2 * 100 + 4 * 70;
+        System.out.println("в граммах = " + Breakfast + " а в килограммах = " + Breakfast / 1000f);
+
+
+        System.out.println("Если худеть по 250гр в день то "+ 7000 / 250 + " дней а если 500гр то "+ 7000 / 500);
+
+
+        int Masha = 67760;
+        int Denis = 83690;
+        int Kristina = 76230;
+
+        System.out.println("Текущая зарплата Маши "+ Masha + " рублей в год это " + Masha * 12 + " после повышения на 10% "+ Masha * 1.1 + " в год это "+ (Masha * 1.1) * 12);
+        System.out.println("Текущая зарплата Маши "+ Denis + " рублей в год это " + Denis * 12 + " после повышения на 10% "+ Denis * 1.1 + " в год это "+ (Denis * 1.1) * 12);
+        System.out.println("Текущая зарплата Маши "+ Kristina + " рублей в год это " + Kristina * 12 + " после повышения на 10% "+ Kristina * 1.1 + " в год это "+ (Kristina * 1.1) * 12);
     }
 }
